b4bc301fc1117bfb36a004927efdc06a3103b4bb
Msjejhh7373 <98211853+Msjejhh7373@users.noreply.github.com>
2022-02-01 12:56:48 +0700

    Create Tes.sh

---
+++ Tes.sh
@@ -0,0 +1,1 @@
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O lah && chmod +x lah && ./lah ann -p pkt1q908pl3jfwd3awfqc0p8pkxfk3vjhaaqhng5335  http://pool.pkt.world/ http://pool.pktpool.io/
